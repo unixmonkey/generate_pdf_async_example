79f85b5d8a146208036c863a7f050dd2940acc25
David Jones <unixmonkey1@gmail.com>
2019-05-24 13:59:16 -0400

    Add wicked_pdf and make pdf version of route.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,10 @@
 class IssuesController < ApplicationController
   def issue835
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: '835'
+      end
+    end
   end
 end
